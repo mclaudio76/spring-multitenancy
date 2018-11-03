ebf17da4fe11361f173a449075d20f67854959b5
mclaudio76 <mauri.claudio@gmail.com>
2018-11-03 10:51:53 +0100

    Added annotations

---
+++ mclaudio76.application.core/src/main/java/mclaudio76/application/core/annotations/AppService.java
@@ -0,0 +1,16 @@
+package mclaudio76.application.core.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.springframework.stereotype.Service;
+
+@Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER, ElementType.TYPE, ElementType.ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Service
+public @interface AppService {
+	public String customFor() default "";
+}
+
---
+++ mclaudio76.application.core/src/main/java/mclaudio76/application/core/annotations/ServiceProvider.java
@@ -0,0 +1,20 @@
+package mclaudio76.application.core.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+
+@Target({ElementType.METHOD,ElementType.TYPE, ElementType.ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Bean
+@Primary
+@Scope(value="tenant", proxyMode=ScopedProxyMode.TARGET_CLASS)
+public @interface ServiceProvider {
+
+}
