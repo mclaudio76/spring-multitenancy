8a33fd51906c206540db6d623eb988f5fdb8156d
mclaudio76 <mauri.claudio@gmail.com>
2018-11-03 10:48:41 +0100

    Changed

--- mclaudio76.application.core/src/main/java/mclaudio76/application/core/services/IProductService.java
+++ mclaudio76.application.core/src/main/java/mclaudio76/application/core/services/IProductService.java
@@ -7,4 +7,4 @@ import mclaudio76.application.core.entities.Product;
 public interface IProductService {
 	public ArrayList<Product> findByFilter(Product filter);
 	public void saveProduct(Product item);
-}
+} 
