f3176407538d28320917fd033d3acdbf8c6ab9da
mclaudio76 <mauri.claudio@gmail.com>
2018-11-04 17:45:50 +0100

    Adding support for JTA

---
+++ mclaudio76.application.main/src/main/java/mclaudio76/application/jpa/ApplicationJtaPlatform.java
@@ -0,0 +1,34 @@
+package mclaudio76.application.jpa;
+
+import java.util.logging.Logger;
+
+import javax.transaction.TransactionManager;
+import javax.transaction.UserTransaction;
+
+import org.hibernate.engine.transaction.jta.platform.internal.AbstractJtaPlatform;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class ApplicationJtaPlatform extends AbstractJtaPlatform {
+
+	private static final long serialVersionUID = 1L;
+	
+	private static TransactionManager txManager;
+	private static UserTransaction    userTx;
+	
+	@Autowired
+	public ApplicationJtaPlatform(TransactionManager txManager, UserTransaction userTx) {
+		ApplicationJtaPlatform.txManager = txManager;
+		ApplicationJtaPlatform.userTx 	 = userTx;
+		Logger.getLogger(this.getClass().getCanonicalName()).info("ApplicationJtaPlatform wired up");
+	}
+	
+	@Override
+	protected TransactionManager locateTransactionManager() {
+		return txManager;
+	}
+
+	@Override
+	protected UserTransaction locateUserTransaction() {
+		return userTx;
+	}
+}
